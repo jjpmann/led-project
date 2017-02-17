51165d40f13e9cf3fd6754c971090de946298590
Jerry Price <jerryprice@lmo.com>
2017-02-16 21:44:42 -0500

    updates

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ controller1 = MagicHomeApi('192.168.1.31', 0)
 
 
 
-#controller1.update_device(255, 0, 0)
+controller1.update_device(255, 0, 0)
 #controller1.update_device(0, 255, 0)
 #controller1.update_device(10, 10, 250)
 
-controller1.turn_on()
+#controller1.turn_on()
